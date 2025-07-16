1fb51800f588811cca0b989d883975bfed0f05ff
Lahcen Idali <lehcenidali6@gmail.com>
2025-07-16 12:34:58 +0100

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 import { app ,server} from "./lib/socket.js";
 
 dotenv.config();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://conversa-xi.vercel.app", credentials: true }));
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
